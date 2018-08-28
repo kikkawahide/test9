3afbb69cf4e66965e40f6559ef2e1c8d41c6cca9
kikkawahide <kikkawa9512@gmail.com>
2018-08-28 16:12:52 +0900

    update2

--- app/better-form/better-form.component.ts
+++ app/better-form/better-form.component.ts
@@ -23,5 +23,17 @@ export class BetterFormComponent implements OnInit {
   }
   get fieldOne(){return this.calcForm.get("fieldOne");}
   get fieldTwo(){return this.calcForm.get("fieldTwo");}
+  addAnyway(){
+    let text1:string = this.fieldOne.value;
+    let text2:string = this.fieldTwo.value;
+    let resultStr:string = "";
+
+    if (Number.isNaN(Number(text1))  || Number.isNaN(Number(text2)) )  {
+      resultStr = text1 + text2 ;
+    } else {
+      resultStr = `${text1}+${text2}=${Number(text1)+Number(text2)}`;
+    }
+    this.result = resultStr;
+  }
 
 }
